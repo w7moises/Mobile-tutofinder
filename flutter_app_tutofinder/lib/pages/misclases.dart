e91833febe8f12f0fb911901927f10ec3656f98d
carlostarazona <carlostarazona887@hotmail.com>
2020-11-27 05:34:17 -0500

    Buscar clase no funcional

--- flutter_app_tutofinder/lib/pages/misclases.dart
+++ flutter_app_tutofinder/lib/pages/misclases.dart
@@ -1,3 +1,4 @@
+import 'dart:async';
 import 'dart:convert';
 
 import 'package:flutter/material.dart';
@@ -8,9 +9,26 @@ class MisClases extends StatefulWidget {
   _MisClasesState createState() => _MisClasesState();
 }
 
+class Debouncer {
+  final int milliseconds;
+  VoidCallback action;
+  Timer _timer;
+
+  Debouncer({this.milliseconds});
+
+  run(VoidCallback action) {
+    if (null != _timer) {
+      _timer.cancel();
+    }
+    _timer = Timer(Duration(milliseconds: milliseconds), action);
+  }
+}
+
 class _MisClasesState extends State<MisClases> {
   String url = 'https://tutofinder-movil.herokuapp.com/tutorias';
+  final _debouncer = Debouncer(milliseconds: 500);
   List data;
+  List filtro;
 
   Future<String> makeRequest() async{
     var response = await http.get(Uri.encodeFull(url));
@@ -18,13 +36,23 @@ class _MisClasesState extends State<MisClases> {
     setState(() {
       var extractdata = json.decode(response.body);
       data = extractdata;
+      filtro = data;
     });
   }
 
   @override
   void initState(){
-    this.makeRequest();
+    super.initState();
+    this.makeRequest().then((value){
+      setState(() {
+        data = json.decode(value);
+        filtro = data;
+      });
+    });
+
   }
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -39,19 +67,40 @@ class _MisClasesState extends State<MisClases> {
               onPressed: () {})
         ],
       ),
-      body: ListView.builder(
-          itemCount: data == null ? 0 : data.length,
-          itemBuilder: (BuildContext context, i){
-            return ListTile(
-              title: Text('Curso: ' + data[i]['curso']['nombre']),
-              subtitle: Text('Tiempo: ' + data[i]['cantidadMinutos'].toString() + ' minutos'),
-              trailing: Icon(Icons.comment),
-              leading: Icon(Icons.account_circle),
-              onTap: (){
+      body: Column(
+        children: <Widget>[
+          TextField(
+            decoration: InputDecoration(
+                contentPadding: EdgeInsets.all(15.0),
+                hintText: 'Filter by name or name'),
+            onChanged: (string) {
+              _debouncer.run(() {
+                setState(() {
+                  filtro = data
+                      .where(
+                          (u) => (u..toLowerCase().contains(string.toLowerCase()))).toList();
+                });
+              });
 
-              },
-            );
-          }),
+            },
+          ),
+          Expanded(
+            child:ListView.builder(
+                itemCount: filtro == null ? 0 : filtro.length,
+                itemBuilder: (BuildContext context, i){
+                  return ListTile(
+                    title: Text('Curso: ' + filtro[i]['curso']['nombre']),
+                    subtitle: Text('Tiempo: ' + filtro[i]['cantidadMinutos'].toString() + ' minutos'),
+                    trailing: Icon(Icons.comment),
+                    leading: Icon(Icons.account_circle),
+                    onTap: (){
+
+                    },
+                  );
+                }),
+          ),
+        ],
+      ),
     );
   }
 }
